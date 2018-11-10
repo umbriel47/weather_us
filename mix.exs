8c6f229d4bbe7e5a4d13798461505ef4060e8bba
umbriel <lplai47@hotmail.com>
2018-11-10 09:12:34 +0800

    add doc support by ex_doc

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule WeatherUs.MixProject do
     [
       app: :weather_us,
       version: "0.1.0",
+      name: "WeatherUS",
+      source_url: "https://github.com/umbriel/weather_us",
       elixir: "~> 1.6",
       escript: escript_config,
       start_permanent: Mix.env() == :prod,
@@ -23,7 +25,8 @@ defmodule WeatherUs.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.4"},
-      {:sweet_xml, "~> 0.6.5"}
+      {:sweet_xml, "~> 0.6.5"},
+      {:ex_doc, github: "elixir-lang/ex_doc"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
